5be934720ed12bdd762bebb43ef7a506cdbea08b
Samuel Menigat <samuel.menigat@gmail.com>
2025-07-23 19:37:49 +0200

    feat(renderer): do not overwrite size properties

--- src/index.tsx
+++ src/index.tsx
@@ -27,12 +27,6 @@ const ZplRenderer: FC<ZplRendererProps> = ({
       const parsedZpl = parser.parse();
 
       if (parsedZpl.label && canvasRef.current) {
-        // Clear the canvas before rendering using the props dimensions
-        const ctx = canvasRef.current.getContext("2d");
-        if (ctx) {
-          ctx.clearRect(0, 0, width, height);
-        }
-
         // Render the ZPL content (this may modify canvas styles)
         const zplRenderer = new ZPLRenderer(canvasRef.current);
         zplRenderer.render(parsedZpl.label);
@@ -55,15 +49,7 @@ const ZplRenderer: FC<ZplRendererProps> = ({
     }
   }, [zpl, width, height]);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      data-component={dataComponent}
-      width={width}
-      height={height}
-      {...props}
-    />
-  );
+  return <canvas ref={canvasRef} data-component={dataComponent} {...props} />;
 };
 
 export default ZplRenderer;
